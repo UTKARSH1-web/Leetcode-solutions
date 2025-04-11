ba6f6d4c50b06d9a9ffb50dfd34501615f57c234
Harianant Sinha <156214180+Hsinha11@users.noreply.github.com>
2025-04-11 15:05:45 +0530

    Time: 3 ms (89.28%) | Memory: 19.2 MB (41.57%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,23 +1,22 @@
 class Solution:
-    def findCircleNum(self, road: List[List[int]]) -> int:
+    def findCircleNum(self, mat: List[List[int]]) -> int:
         def dfs(node):
-            vis[node] = 1
+            vis[node]=True
             for i in adj[node]:
-                if not vis[i]:
+                if vis[i]==False:
                     dfs(i)
-
-        n = len(road)
+        n = len(mat)
         adj = [[] for _ in range(n)]
-        vis = [0]*n
-        for i in range(n):
-            for j in range(n):
-                if road[i][j] ==1:
-                    adj[i].append(j)
         c=0
         for i in range(n):
-            if vis[i]==0:
-                c+=1
+            for j in range(n):
+                if mat[i][j]==1:
+                    adj[i].append(j)
+        vis = [False]*(n)
+        for i in range(n):
+            if not vis[i]:
                 dfs(i)
-        
+                c+=1
         return c
-
+        
+            
