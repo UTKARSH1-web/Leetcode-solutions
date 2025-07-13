13e67c4757d8c0494a21fb46886e1c0102cc3836
Harianant Sinha <156214180+Hsinha11@users.noreply.github.com>
2025-07-13 13:26:55 +0530

    Time: 644 ms (48.28%) | Memory: 42.6 MB (61.19%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
@@ -1,22 +1,14 @@
 class Solution:
-    def f(self,spell,potions,success):
-        i = 0
-        n =len(potions)
-        j = len(potions)
-        ans=0
-        while i<j:
-            mid = i + (j-i)//2
-            if potions[mid]*spell >=success:
-                # ans = max(n - mid, ans)
+    def f(self, spell, potions, success):
+        i, j = 0, len(potions)
+        while i < j:
+            mid = (i + j) // 2
+            if potions[mid] * spell >= success:
                 j = mid
             else:
-                i=mid+1
-        return n - i
+                i = mid + 1
+        return len(potions) - i
 
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        n = len(spells)
-        ans = [0]*n
         potions.sort()
-        for i in range(n):
-            ans[i] = self.f(spells[i],potions,success)
-        return ans
+        return [self.f(spell, potions, success) for spell in spells]
