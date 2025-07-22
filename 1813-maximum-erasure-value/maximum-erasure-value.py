d0d9c07ebeb55c08cae5bca524c066a50173bc4f
Harianant Sinha <156214180+Hsinha11@users.noreply.github.com>
2025-07-22 08:31:59 +0530

    Time: 223 ms (53.80%) | Memory: 28.8 MB (50.60%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.py
+++ 1813-maximum-erasure-value/maximum-erasure-value.py
@@ -4,13 +4,16 @@ class Solution:
         i=0
         ans=0
         j=0
+        curr=0
         n= len(nums)
         while j<n:
             if nums[j] not in a:
                 a.add(nums[j])
-                ans=max(ans,sum(a))
+                curr+=nums[j]
+                ans=max(ans,curr)
                 j+=1
             else:
                 a.remove(nums[i])
+                curr-=nums[i]
                 i+=1
         return ans
