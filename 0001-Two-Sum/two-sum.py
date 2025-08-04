a194af157f958ecaef288a005b7029a6ce214c5a
Harianant Sinha <156214180+Hsinha11@users.noreply.github.com>
2025-08-04 21:09:18 +0530

    Delete 0001-Two-Sum directory

--- 0001-Two-Sum/two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums, target: int):
-        visited = {}
-        for i in range(len(nums)):
-            difference = target - nums[i]
-            if difference in visited:
-                return [visited[difference], i]
-            else:
-                visited[nums[i]] = i
