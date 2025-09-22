03d6930f87092fec46467622f6ef850667111e97
Harianant Sinha <156214180+Hsinha11@users.noreply.github.com>
2025-09-22 17:45:50 +0530

    Time: 25 ms (88.81%) | Memory: 20.2 MB (92.95%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,12 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         d={}
+        cnt=0
         d[0]=1
-        presum =0
-        cnt =0
-        for i in range(len(nums)):
-            presum+=nums[i]
-            remove = presum -k
-            cnt +=d.get(remove,0)
-            d[presum] = d.get(presum,0)+1
+        remove=0
+        presum=0
+        for i in nums:
+            presum+=i
+            remove = presum-k
+            cnt+=d.get(remove,0)
+            d[presum] = d.get(presum,0) +1
         return cnt
